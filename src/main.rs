03ded9e804e4f9fba18d378e3097c51673bfb007
kento <kento@toriwatari.bird>
2023-04-01 17:17:24 +0900

    ✨ Update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::env;
+use sha1::{Sha1, Digest};
+
+/*
 use std::fs;
 use std::io;
 
@@ -8,12 +12,32 @@ use cmd::{init, add};
 use object::{git_object, blob};
 
 use blob::Blob;
+*/
 
 fn main() {
+    let blob = format!("blob {}\0{}", 11, "Hello World");
+
+    println!("{}", blob);
+
+    let blob_object = Sha1::digest(blob.as_bytes());
+
+    println!("{:x}", blob_object);
+    //=> 5e1c309dae7f45e0f39b1bf3ac3cd9db12e7d689
+
+    let abs_path = env::current_dir();
+
+    match abs_path {
+        Ok(ok) => println!("{}", ok.display()),
+        Err(err) => println!("{}", err)
+    };
+
+    /*
     let args: Vec<String> = std::env::args().collect();
     let cmd = args.get(1).unwrap().clone();
 
-    git_object::test();
+    // let blob = Blob::new("hogehoge".to_string());
+
+    // println!("{:?}", blob);
 
     match cmd.as_str() {
         "init" => {
@@ -36,4 +60,5 @@ fn main() {
             println!("error!");
         }
     }
+    */
 }
